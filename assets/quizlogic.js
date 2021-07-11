4c7d4969a79cda6d62282d06d1a4f1819e41cbff
Mike Rogers <mrogers23458@gmail.com>
2021-07-11 08:54:34 -0700

    created js function renderQuestions that rendes a question answer box when start button is pushed

--- assets/quizlogic.js
+++ assets/quizlogic.js
@@ -1,6 +1,28 @@
 let startButton = document.getElementById('startBtn')
+
+let secondsLeft = 10
+let timer = document.querySelector('.time')
+let messageScore = function(){
+    console.log('works after timer expires')
+}
+
 function beginGame(){
-    console.log('success')
+    let timerInterval = setInterval(function(){
+        secondsLeft--;
+        timer.textContent = "Time left: " + secondsLeft
+
+        if(secondsLeft ===0){
+            clearInterval(timerInterval)
+            messageScore()
+        }
+
+    }, 1000);
+
+    let renderQuestions = function(){
+        document.querySelector('.questions').setAttribute('style', 'display: flex;')
+    }
+    renderQuestions();
+
 }
 
 
