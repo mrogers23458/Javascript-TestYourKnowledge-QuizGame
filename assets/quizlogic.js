234e1f34eebb97081657951d240ff89861598f91
Mike Rogers <mrogers23458@gmail.com>
2021-07-21 08:41:08 -0700

    updated js quiz logic that set attribute for scores from display flex, to display block. This prevented quiz box from jumping to the right after save score was clicked. Additionally updated readme file. Also added a screenshot to repository to add back into readme.

--- assets/quizlogic.js
+++ assets/quizlogic.js
@@ -1,3 +1,4 @@
+let body = document.body
 let startButton = document.getElementById('startBtn')
 let qDisplay = document.getElementById('qContent')
 let pickA = document.getElementById('optionA')
@@ -5,16 +6,38 @@ let pickB = document.getElementById('optionB')
 let pickC = document.getElementById('optionC')
 let pickD = document.getElementById('optionD')
 let score = 0
+let secondsLeft = 90
+let save = document.getElementById('saveScore')
+let initialsInput = document.getElementById('initials')
+let initialsSaved = ''
+let scoreSaver = document.querySelector('.highScore')
+let highScores = document.querySelector('.test')
+
+
+function saveclick(event){
+    event.preventDefault()
+    
+
+    initialsSaved = initialsInput.value
+    scoreSaver.setAttribute('style', 'display: block;')
+
+    let addScore = document.createElement('li')
+    addScore.textContent = initialsSaved
+
+    highScores.appendChild(addScore)
+
+
+}
 
 let messageScore = function(){
-    document.querySelector('.scoreOverlay').setAttribute('style', 'display: block;')
+    document.querySelector('.scoreOverlay').setAttribute('style', 'display: flex;')
     document.getElementById('numberCorrect').textContent = score;
     document.getElementById('numberWrong').textContent = ((qArray.length)-(score))
     
 }
 
 function renderQuestions(){
-    document.querySelector('.questions').setAttribute('style', 'display: flex;')
+    document.querySelector('.questions').setAttribute('style', 'visibility: visible;')
     currenQuestion = qArray[setQuestion]
     qDisplay.textContent = currenQuestion.question
     pickA.textContent = currenQuestion.choiceA
@@ -54,18 +77,19 @@ let qArray = [
 
 let setQuestion = 0
 let currenQuestion = []
-console.log(currenQuestion)
+
 
 function checkAnswer(value){
     
     currenQuestion = qArray[setQuestion]
     
     if(value === currenQuestion.correct) {
-        console.log('correct');
         setQuestion ++;
         score ++;
-    }else {(console.log('wrong'))
-        setQuestion ++;}
+    }else {
+        setQuestion ++;
+        secondsLeft = secondsLeft -(10);
+    }
 
     if(setQuestion == qArray.length){
         messageScore();
@@ -73,8 +97,7 @@ function checkAnswer(value){
         return;
     }
     
-    console.log(setQuestion)
-    console.log(currenQuestion)
+  
 
     renderQuestions();
     
@@ -84,7 +107,7 @@ function checkAnswer(value){
 let timer = document.querySelector('.time')
 
 function beginGame(){
-    let secondsLeft = 20
+    
     document.querySelector('.scoreOverlay').setAttribute('style', 'display: none;')
     setQuestion = 0
     score = 0
@@ -108,6 +131,5 @@ function beginGame(){
     
 }
 
-
+save.addEventListener('click', saveclick)
 startButton.addEventListener('click', beginGame)
-console.log(setQuestion)
---
+++ readme.md
@@ -0,0 +1,17 @@
+## Javascript Quiz Game with questions about Javascript
+## Description
+The goal of this project was to use Javascript to create a timed quiz game, where users would have to answers questions about Javascript within the allotted time. If the user was unable to answer all of the questions within the allotted time, the game will time out and they will be prompted to save their score. The score is based on the total number of correct answers. The game also tracks number of incorrect answers.
+
+During this project I learned and applied a lot of Javascript. This included setting global and local variables, arrays, functions, if and if/else statements. Additionally I learned how to traverse the dom and use Javascript associated functions such as getElementById, querySelector, appendChild. I also learned how to create a timer using setInterval and values in ms. Finally, I learned how to use Javascript to update CSS elements which allowed me to hide portions of the quiz such as the questions and scoreboard until a click action happened. 
+## Usage
+To play the game click the Start Quiz button. This will begin a timer that gives 90 seconds to answer 3 questions about Javascript. Be careful when you answer though a wrong answer will deduct 10seconds from the timer.
+## Screenshot
+![project](https://github.com/mrogers23458/password-generator/blob/master/assets/SSforreadme.jpg)
+[Deployed project available here](https://mrogers23458.github.io/password-generator/)
+
+## Credits
+
+Thank you to Treighton Mauldin, Chris Song, Ryan Skog for all of your instruction over the past week. It's been a pleasure to learn Javascript from/with you!
+## License
+
+CC0 1.0 Universal
